f6efab216fb61ea3cb51a3ca8b863c7080711c19
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-07-21 12:44:08 +0200

    Added drafts publication. Added houston mongo admin interface.

--- server/models/posts/publications.js
+++ server/models/posts/publications.js
@@ -79,7 +79,45 @@ Meteor.publish("latestPosts", function (limit) {
 			title: 1,
 			subtitle: 1,
 			authors: 1,
-			publishedOn: 1
+			publishedOn: 1,
+			published: 1
+		}
+	};
+	// Return the cursor finding those posts
+	return Posts.find(selector, options);
+});
+
+
+
+Meteor.publish("postDrafts", function (limit) {
+	// Get the current user
+	var user = Meteor.users.findOne({_id: this.userId}) || {};
+	// Get the permissions selector
+	var permissionsSelector = PermissionsEnum.Posts.getPermissionsSelector(user);
+	// Construct the selector
+	var selector = {
+		$and: [
+			{
+				// Select only published posts
+				published: false
+			},
+			permissionsSelector
+		]
+	};
+	// Sanitize the limit
+	limit = parseInt(limit, 10);
+	limit = isNaN(limit) ? 10 : limit;
+	var options = {
+		limit: limit,
+		sort: {
+			publishedOn: -1
+		},
+		fields: {
+			title: 1,
+			subtitle: 1,
+			authors: 1,
+			publishedOn: 1,
+			published: 1
 		}
 	};
 	// Return the cursor finding those posts
