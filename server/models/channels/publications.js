9eccafea7fc5bebc2037b307b13266619a6df5cd
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-08-04 10:29:07 +0200

    Added search publication

--- server/models/channels/publications.js
+++ server/models/channels/publications.js
@@ -22,3 +22,34 @@ Meteor.publish("singleChannel", function (idOrName) {
 	// Return the cursor finding that post
 	return Channels.find(selector);
 });
+
+Meteor.publish("channelsByFuzzyName", function (name, limit) {
+	// Get the current user
+	var user = Meteor.users.findOne({_id: this.userId}) || {};
+	// Get the permissions selector
+	var permissionsSelector = PermissionsEnum.Channels.getPermissionsSelector(user);
+	// Construct the selector
+	var selector = {
+		$and: [
+			{
+				name: new RegExp(name, "i"),
+				published: true
+			},
+			permissionsSelector
+		]
+	};
+	// Sanitize the limit
+	limit = parseInt(limit, 10);
+	limit = isNaN(limit) ? 10 : limit;
+	var options = {
+		limit: limit,
+		fields: {
+			name: 1,
+			commonName: 1,
+			curators: 1,
+			published: 1
+		}
+	};
+	// Return the cursor finding that post
+	return Channels.find(selector, options);
+});
